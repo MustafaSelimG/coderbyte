9f057db364452543a7cb0035f8a880c02201071c
Mustafa Selim Gunaydin <mustafaselimgunaydin@gmail.com>
2021-11-09 12:18:13 +0300

    Update Fibonacci.java

--- Patika/Fibonacci.java
+++ Patika/Fibonacci.java
@@ -2,8 +2,8 @@
 
     public class Prime {
         public static void main(String[] args) {
-            int num1=1,num2=1,temp;
-            for (int i = 1; i <= 7; i++) {
+            int num1=0,num2=1,temp;
+            for (int i = 1; i <= 8; i++) {
                 temp = num1+num2;
                 System.out.println(num1 +" + "+ num2+" = "+temp);
                 num1=num2;
